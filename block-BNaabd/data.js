ba8c39a52b30bb8626dbf51f377f82eda9a9defa
anup kumar <kanup414@gmail.com>
2022-12-07 00:00:20 +0530

    done

--- block-BNaabd/data.js
+++ block-BNaabd/data.js
@@ -1,22 +1,24 @@
 var http = require('http');
 
+var qs = require('querystring');
 var server = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
     var store = '';
-    var dataFormat = req.headers['content-type'];
+    // var dataFormat = req.headers['content-type'];
     req.on('data', (chunk) => {
         store += chunk;
     });
 
     req.on('end', () => {
-        if(dataFormat === "application/json") {
-            var parsedData = JSON.parse(store);
+        if(req.method === "POST" && req.url === '/json') {
+            res.setHeader('content-Type', "application/json")
             res.end(store);
         }
-
-        if(dataFormat === "application/x-www-form-urlencoded") {
-            qs.parse(JSON.stringify(parsedData))
+        if(req.method === "POST" && req.url === '/json') {
+          console.log(store);
+          var formData = qs.parse(store);
+          res.end(JSON.stringify(formData))
         }
     })
 }
